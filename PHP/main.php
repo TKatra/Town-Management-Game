4a56f99b920d5ca34058cac95f9c3104fed5a299
TKatra <tobiaskatra@hotmail.com>
2014-12-26 16:05:33 +0100

    tests for checking the getters and setters for towns

--- PHP/main.php
+++ PHP/main.php
@@ -1,4 +1,23 @@
 <?php
 include_once("../libs/nodebite-swiss-army-oop.php");
-$dirtyTown = new SmallVillage("Dirty Town");
 
+$town = new SmallVillage("Small Town");
+
+var_dump($town);
+echo "<br>Set too high values<br>";
+$town->setFood(256);
+$town->setHappiness(1452);
+$town->setMoney(55555);
+$town->setEducation(1342342);
+$town->setMilitary(32498);
+$town->setPopulation(2344);
+var_dump($town);
+echo "<br>Set too low values<br>";
+$town->setFood(-256);
+$town->setHappiness(-1452);
+$town->setMoney(-55555);
+$town->setEducation(-1342342);
+$town->setMilitary(-32498);
+$town->setPopulation(-2344);
+var_dump($town);
+?>
