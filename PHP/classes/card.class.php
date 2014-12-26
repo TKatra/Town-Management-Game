37d0db37574fb2da304e6db089bc180a643e8e7e
TKatra <tobiaskatra@hotmail.com>
2014-12-26 14:09:26 +0100

    created Card class

---
+++ PHP/classes/card.class.php
@@ -0,0 +1,15 @@
+<?php
+class Card
+{
+	protected $title;
+	protected $description;
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+	public function getDescription()
+	{
+		return $this->description;
+	}
+}
