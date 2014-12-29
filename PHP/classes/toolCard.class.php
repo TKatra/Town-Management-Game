c6506a67d12b384fd62e2980c6231cdde8e12e77
TKatra <tobiaskatra@hotmail.com>
2014-12-29 18:20:52 +0100

    Fields and getters to tool cards

--- PHP/classes/toolCard.class.php
+++ PHP/classes/toolCard.class.php
@@ -1,5 +1,27 @@
 <?php
 class ToolCard extends Card
 {
-	
+	protected $targetSelf; //bool
+	protected $cost; //associativ array (Food = -20)
+	protected $selfEffect; //associativ array (Food => 20)
+	protected $opponentEffect; //associativ array
+
+
+
+	public function getTargetSelf()
+	{
+		return $this->targetSelf;
+	}
+	public function getCost()
+	{
+		return $this->cost;
+	}
+	public function getSelfEffect()
+	{
+		return $this->targetSelf;
+	}
+	public function getOpponentEffect()
+	{
+		return $this->opponentEffect;
+	}
 }
