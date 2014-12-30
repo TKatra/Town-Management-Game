391ed229a7e55b3351c42c8b9dec5037f86b10a7
TKatra <tobiaskatra@hotmail.com>
2014-12-30 14:30:46 +0100

    Added getters for the event card class

--- PHP/classes/eventCard.class.php
+++ PHP/classes/eventCard.class.php
@@ -6,4 +6,25 @@ class EventCard extends Card
 	protected $winEffect; //Effect
 	protected $loseEffect; //Effect
 	protected $startupEffect; //Effect
+
+	public getWinCondition()
+	{
+		return $this->winCondition;
+	}
+	public getLoseCondition()
+	{
+		return $this->loseCondition;
+	}
+	public getWinEffect()
+	{
+		return $this->winEffect;
+	}
+	public getLoseEffect()
+	{
+		return $this->loseEffect;
+	}
+	public getStartupEffect()
+	{
+		return $this->startupEffect;
+	}
 }
