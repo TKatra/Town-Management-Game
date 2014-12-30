6796b471b628f803850f1258e63acdd000621eb5
TKatra <tobiaskatra@hotmail.com>
2014-12-30 17:02:53 +0100

    I commented out the tests about towns, since they would just crash because the classes it worked with doesn't exist anymore

--- PHP/main.php
+++ PHP/main.php
@@ -1,24 +1,34 @@
 <?php
 include_once("../libs/nodebite-swiss-army-oop.php");
 
-$town = new SmallVillage("Small Town");
+// $town = new SmallVillage("Small Town");
 
-var_dump($town);
-echo "<br>Set too high values<br>";
-$town->setFood(256);
-$town->setHappiness(1452);
-$town->setMoney(55555);
-$town->setEducation(1342342);
-$town->setMilitary(32498);
-$town->setPopulation(2344);
-var_dump($town);
-echo "<br>Set too low values<br>";
-$town->setFood(-256);
-$town->setHappiness(-1452);
-$town->setMoney(-55555);
-$town->setEducation(-1342342);
-$town->setMilitary(-32498);
-$town->setPopulation(-2344);
-var_dump($town);
-echo "<br>";
+// var_dump($town);
+// echo "<br>Set too high values<br>";
+// $town->setFood(256);
+// $town->setHappiness(1452);
+// $town->setMoney(55555);
+// $town->setEducation(1342342);
+// $town->setMilitary(32498);
+// $town->setPopulation(2344);
+// var_dump($town);
+// echo "<br>Set too low values<br>";
+// $town->setFood(-256);
+// $town->setHappiness(-1452);
+// $town->setMoney(-55555);
+// $town->setEducation(-1342342);
+// $town->setMilitary(-32498);
+// $town->setPopulation(-2344);
+// var_dump($town);
+// echo "<br>";
+
+$testArray = array(
+	1 => "dfgrdse",
+	"super" => "Man",
+	45 => 648,
+	404 => "Not found. DERP!",
+	"QWERTY" => 1234567
+	);
+var_dump($testArray);
+// foreach ()
 ?>
