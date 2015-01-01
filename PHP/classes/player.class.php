9442f00b8654103042eb391c779781a102fff0e4
TKatra <tobiaskatra@hotmail.com>
2015-01-01 18:13:03 +0100

    added a field for discarded tool cards and a getter and a setter for it

--- PHP/classes/player.class.php
+++ PHP/classes/player.class.php
@@ -1,11 +1,12 @@
 <?php
 class Player
 {
-	protected $name; //string
+	protected $name; //string 
 	protected $town; //town
 	protected $toolCards; //array of ToolCards
+	protected $discardedToolCards; //array of discarded ToolCards
 
-	public function __construct($name, $town)
+	public function __construct($name, $town) //string, Town 
 	{
 		$this->name = $name;
 		$this->town = $town;
@@ -23,8 +24,17 @@ class Player
 	{
 		return $this->toolCards;
 	}
+	public function getDiscardedToolCards()
+	{
+		return $this->discardedToolCards;
+	}
+
 	public function setToolCards($toolCards)
 	{
 		$this->toolCards = $toolCards;
+	}
+	public function setDiscardedToolCards($discardedToolCards)
+	{
+		$this->discardedToolCards = $discardedToolCards;
 	}
 }
