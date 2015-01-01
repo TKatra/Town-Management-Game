80a3a9c12c07409c9642282c155b51d4ddc922ed
TKatra <tobiaskatra@hotmail.com>
2015-01-01 15:51:48 +0100

    made some changes to the condition class. I also gave it some functions to check the string it receives so it works with the things I want it to work with

--- PHP/classes/condition.class.php
+++ PHP/classes/condition.class.php
@@ -1,31 +1,86 @@
 <?php
 class Condition
 {
-	protected $winType; //string
-	protected $loseType; //string
-	protected $winValue; //int
-	protected $loseValue; //int
+	protected $conditionType; //string
+	protected $statsType; //string
+	protected $value; //int
+	
 
 //om databaskall returnerar associativa arrayer mata in en sån i construktor
-	public function __construct()
+	public function __construct($conditionDetails) 
 	{
-		
+		//highestOfPlayers
+		//lowestOfPlayers
+		//
+		//moreThanValue
+		//lessThanValue
+
+		$this->checkType($conditionDetails["conditionType"]);
+		$this->checkStatsType($conditionDetails["statsType"]);
+
+		if($conditionDetails["value"] > 100)
+		{
+			$conditionDetails["value"] = 100;
+		}
+		else if($conditionDetails["value"] < -100)
+		{
+			$conditionDetails["value"] = -100;
+		}
+
+		$this->conditionType = $conditionDetails["conditionType"];
+		$this->statsType = $conditionDetails["statsType"];
+		$this->value = (int)$conditionDetails["value"];
 	}
 
-	public function getWinType()
+	public function getConditionType()
 	{
-		return $this->winType;
+		return $this->conditionType;
 	}
-	public function getLoseType()
+	public function getValue()
 	{
-		return $this->loseType;
+		return $this->value;
 	}
-	public function getWinValue()
+	public function getStatsType()
 	{
-		return $this->winValue;
+		return $this->statsType;
 	}
-	public function getLoseValue()
+
+	private function checkType($conditionType)
 	{
-		return $this->loseValue;
+		switch ($conditionType) 
+		{
+			case "highestOfPlayers":
+				break;
+			case "lowestOfPlayers":
+				break;
+			case "moreThanValue":
+				break;
+			case "lessThanValue":
+				break;
+			default:
+				throw new Exception("'".$conditionType."' is an incorrect condition type!");
+				break;
+		}
+	}
+	protected function checkStatsType($statsType)
+	{
+		switch ($statsType) 
+		{
+			case "food":
+				break;
+			case "happiness":
+				break;
+			case "money":
+				break;
+			case "education":
+				break;
+			case "military":
+				break;
+			case "population":
+				break;
+			default:
+				throw new Exception("'".$statsType."' is an incorrect stats type!");				
+				break;
+		}
 	}
 }
