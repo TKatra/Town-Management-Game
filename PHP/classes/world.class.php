9cc77bc9663c0747a54e0a0dc6575d010e7aa504
TKatra <tobiaskatra@hotmail.com>
2015-01-05 10:43:51 +0100

    Started building the world class

--- PHP/classes/world.class.php
+++ PHP/classes/world.class.php
@@ -1,5 +1,48 @@
 <?php
 class World
 {
-	
+	protected $playerQueue = array();
+	protected $toolDeck = array();
+	protected $toolDiscardDeck = array();
+	protected $eventDeck = array();
+	protected $eventDiscardDeck = array();
+
+	public function __construct($players)//, $toolDeck, $eventDeck array, array, array
+	{
+		$this->playerQueue = $players;
+		// $this->toolDeck = $toolDeck;
+		// $this->eventDeck = $eventDeck;
+	}
+
+	public function getPlayerQueue()
+	{
+		return $this->playerQueue;
+	}
+	public function getToolDeck()
+	{
+		return $this->toolDeck;
+	}
+	public function getEventDeck()
+	{
+		return $this->eventDeck;
+	}
+
+	//REMOVE THESE AFTER TESTS!!
+	////////////////////////////////////////
+	public function getToolDiscardDeck()
+	{
+		return $this->toolDiscardDeck;
+	}
+	public function getEventDiscardDeck()
+	{
+		return $this->eventDiscardDeck;
+	}
+	////////////////////////////////////////
+
+	public function changePlayerOrder()
+	{
+		$this->playerQueue[] = $this->playerQueue[0];
+		unset($this->playerQueue[0]);
+		$this->playerQueue = array_values($this->playerQueue);
+	}
 }
