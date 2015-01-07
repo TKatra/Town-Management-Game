378c5fa75fc2dd49bea54ee72d8cada23cd5458d
TKatra <tobiaskatra@hotmail.com>
2015-01-07 19:52:56 +0100

    Now the test-db creates cards, the world and worts the cards in the world object

--- PHP/test-db.php
+++ PHP/test-db.php
@@ -87,8 +87,16 @@ $DBPlayers = $PDOHelper->query("SELECT * FROM Players");
 $DBPlayerNames = $PDOHelper->query("SELECT * FROM PlayerNames");
 
 $players = array();
-$tempPlayer;
-for ($i = 0; $i < 4; $i++) 
+
+$randomTownIndex = rand(0, count($towns) -1);
+
+$tempPlayer = new Player("Hiero", $towns[$randomTownIndex]);
+$players[] = $tempPlayer;
+
+unset($towns[$randomTownIndex]);
+$towns = array_values($towns);
+
+for ($i = 0; $i < 2; $i++) 
 {
 	$randomNameIndex = rand(0, count($DBPlayerNames) -1);
 	$randomTownIndex = rand(0, count($towns) -1);
@@ -110,9 +118,236 @@ echo"<hr/>";
 
 for ($i = 0; $i < count($players); $i ++) 
 {
-	// var_dump($players[$i]->getName());
-
 	echo("[".$i."] = ".$players[$i]->getName()." the mayor of ".$players[$i]->getTown()->getName()." the ".$players[$i]->getTown()->getType());
+	echo "<br/>";
+}
+
+echo "<hr/>";
+echo "TEST CREATING TOOL CARDS";
+echo "<hr/>";
+
+$DBToolCards = $PDOHelper->query("SELECT * FROM ToolCards");
+var_dump($DBToolCards);
+
+echo "<hr/>";
+
+$toolCards = array();
+
+for ($i = 0; $i < count($DBToolCards); $i++)
+{
+	// $tempToolCard;
+
+	$query = "SELECT Effects.* 
+	FROM ToolCards, Effects 
+	WHERE ToolCards.costEffectID = Effects.ID AND ToolCards.ID = ".($i + 1).";";
+	// var_dump($query);
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBToolCards[$i]["costEffect"] = $DBTempArray[0];
+
+	$query = "SELECT Effects.* 
+	FROM ToolCards, Effects 
+	WHERE ToolCards.selfEffectID = Effects.ID AND ToolCards.ID = ".($i + 1).";";
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBToolCards[$i]["selfEffect"] = $DBTempArray[0];
+
+	$query = "SELECT Effects.* 
+	FROM ToolCards, Effects 
+	WHERE ToolCards.opponentEffectID = Effects.ID AND ToolCards.ID = ".($i + 1).";";
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBToolCards[$i]["opponentEffect"] = $DBTempArray[0];
+
+	// var_dump($DBToolCards[$i]);
+
+	$tempToolCard = new ToolCard($DBToolCards[$i]);
+	// var_dump($tempToolCard);
+
+	$toolCards[] = $tempToolCard;
+	$toolCards[] = $tempToolCard;
+}
+
+var_dump($toolCards);
+
+echo "<hr/>";
+echo "TEST CREATING EVENT CARDS";
+echo "<hr/>";
+
+$DBEventCards = $PDOHelper->query("SELECT * FROM EventCards");
+var_dump($DBEventCards);
+
+echo "<hr/>";
+
+
+
+$eventCards = array();
+// var_dump($DBEventCards[0]["title"]);
+for ($i = 0; $i < count($DBEventCards); $i++)
+{
+
+	// $tempToolCard;
+
+	$query = "SELECT Conditions.* 
+	FROM EventCards, Conditions 
+	WHERE EventCards.winConditionID = Conditions.ID AND EventCards.ID = ".($i + 1).";";
+	// var_dump($query);
+
+	$DBTempArray = $PDOHelper->query($query);
+	// var_dump($DBTempArray[0]);
+	$DBEventCards[$i]["winCondition"] = $DBTempArray[0];
+
+	$query = "SELECT Conditions.* 
+	FROM EventCards, Conditions 
+	WHERE EventCards.loseConditionID = Conditions.ID AND EventCards.ID = ".($i + 1).";";
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBEventCards[$i]["loseCondition"] = $DBTempArray[0];
+
+	$query = "SELECT Effects.* 
+	FROM EventCards, Effects 
+	WHERE EventCards.winEffectID = Effects.ID AND EventCards.ID = ".($i + 1).";";
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBEventCards[$i]["winEffect"] = $DBTempArray[0];
+
+
+	$query = "SELECT Effects.* 
+	FROM EventCards, Effects 
+	WHERE EventCards.loseEffectID = Effects.ID AND EventCards.ID = ".($i + 1).";";
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBEventCards[$i]["loseEffect"] = $DBTempArray[0];
+
+
+	$query = "SELECT Effects.* 
+	FROM EventCards, Effects 
+	WHERE EventCards.startupEffectID = Effects.ID AND EventCards.ID = ".($i + 1).";";
+
+	$DBTempArray = $PDOHelper->query($query);
+	$DBEventCards[$i]["startupEffect"] = $DBTempArray[0];
+
+	// var_dump($DBToolCards[$i]);
+
+	
+	// var_dump($tempToolCard);
+
+	$tempEventCard = new EventCard($DBEventCards[$i]);
+	$eventCards[] = $tempEventCard;
+}
+
+var_dump($eventCards);
+echo "<hr/>";
+echo "TEST TO CREATE WORLD";
+echo "<hr/>";
+
+$world = new World($players, $toolCards, $eventCards);
+var_dump($world);
+
+echo "<hr/>";
+echo "TEST SORTING DIFFERENT CARDS";
+echo "<hr/>";
+echo "TOOL CARDS: NOT SORTED";
+echo "<hr/>";
+
+echo "Tool Deck";
+echo "<br/>";
+
+$testArray = $world->getToolDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "Tool Discard Deck";
+echo "<br/>";
+
+$testArray = $world->getToolDiscardDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "<hr/>";
+echo "TOOL CARDS: SORTED";
+echo "<hr/>";
+
+$world->sortToolDeck();
+
+echo "Tool Deck";
+echo "<br/>";
+
+$testArray = $world->getToolDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "Tool Discard Deck";
+echo "<br/>";
+
+$testArray = $world->getToolDiscardDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "<hr/>";
+echo "EVENT CARDS: NOT SORTED";
+echo "<hr/>";
+
+echo "Event Deck";
+echo "<br/>";
+
+$testArray = $world->getEventDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "Event Discard Deck";
+echo "<br/>";
+
+$testArray = $world->getEventDiscardDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "<hr/>";
+echo "EVENT CARDS: SORTED";
+echo "<hr/>";
+
+$world->sortEventDeck();
+
+$testArray = $world->getEventDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
+	echo "<br/>";
+}
+
+echo "Event Discard Deck";
+echo "<br/>";
+
+$testArray = $world->getEventDiscardDeck();
+
+for ($i = 0; $i < count($testArray); $i++)
+{ 
+	echo("[".$i."] = ".$testArray[$i]->getTitle());
 	echo "<br/>";
 }
 
