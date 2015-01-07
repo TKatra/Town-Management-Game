6198fb66ea27d26e243ddb625aa40cc9278e55b5
TKatra <tobiaskatra@hotmail.com>
2015-01-08 00:20:32 +0100

    added a few things to main.php

--- PHP/main.php
+++ PHP/main.php
@@ -1,4 +1,28 @@
 <?php
 include_once("../libs/nodebite-swiss-army-oop.php");
 
+$connectInfo =array(
+  "host" => "127.0.0.1",
+  "dbname" => "wu14oop2",
+  "username" => "root",
+  "password" => "mysql",
+  "prefix" => "saves"
+  );
+
+$request = $_REQUEST;
+
+$PDOHelper = new PDOHelper($connectInfo["host"], $connectInfo["dbname"], $connectInfo["username"], $connectInfo["password"]);
+$ds = new DBObjectSaver($connectInfo);
+
+if (!isset($ds->players))
+{
+  $ds->players = array();
+}
+
+if (!isset($ds->world))
+{
+  $ds->world = array();
+}
+
+
 ?>
