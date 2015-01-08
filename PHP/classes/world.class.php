bf3e63ffb8a16029902d777a3965015654595090
TKatra <tobiaskatra@hotmail.com>
2015-01-08 16:35:04 +0100

    created some methods that handles the effects and conditions from the current event card

--- PHP/classes/world.class.php
+++ PHP/classes/world.class.php
@@ -60,6 +60,12 @@ class World
 	{
 		$this->dealToolCards();
 		$this->takeAnEventCard();
+
+		for ($i = 0; $i < count($this->playerQueue); $i++)
+		{
+			$this->activateEffect($this->currentEventCard->getStartupEffect(), $this->playerQueue[$i]);
+		}
+
 		$this->currentPlayerTurn = 0;
 	}
 
@@ -70,18 +76,296 @@ class World
 
 		if(!($this->currentPlayerTurn < count($this->playerQueue)))
 		{
-
+			$this->endRound();
 		}
 	}
 
 	protected function endRound()
 	{
+		for ($i = 0; $i < count($this->playerQueue); $i++)
+		{
+			if($this->hasPassedCondition($this->currentEventCard->getWinCondition(),$this->playerQueue[$i]) == true)
+			{
 
+			}
+			else if($this->hasPassedCondition($this->currentEventCard->getLoseCondition(),$this->playerQueue[$i]) == true)
+			{
+
+			}
+		}
 	}
 
 	protected function gameOver()
 	{
 
+	}
+
+	protected function hasPassedCondition($condition, $player)
+	{
+		// $ $this->getStatType()
+		if($condition->getConditionType() == "highestOfPlayers")
+		{
+			if($condition->getStatsType() == "food")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getFood() > $player->getFood())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "happiness")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getHappiness() > $player->getTown()->getHappiness())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "money")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getMoney() > $player->getTown()->getMoney())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "education")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getEducation() > $player->getTown()->getEducation())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "military")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getMilitary() > $player->getTown()->getMilitary())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "population")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getPopulation() > $player->getTown()->getPopulation())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+		else if($condition->getConditionType() == "lowestOfPlayers")
+		{
+			if($condition->getStatsType() == "food")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getFood() < $player->getFood())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "happiness")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getHappiness() < $player->getTown()->getHappiness())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "money")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getMoney() < $player->getTown()->getMoney())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "education")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getEducation() < $player->getTown()->getEducation())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "military")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getMilitary() < $player->getTown()->getMilitary())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+			else if($condition->getStatsType() == "population")
+			{
+				for ($i = 0; $i < count($this->playerQueue); $i++)
+				{
+					if($this->playerQueue[$i]->getTown()->getPopulation() < $player->getTown()->getPopulation())
+					{
+						return false;
+					}
+				}
+				return true;
+			}
+		}
+		else if($condition->getConditionType() == "equalOrMoreThanValue")
+		{
+			if($condition->getStatsType() == "food")
+			{
+				if($player->getFood() >= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "happiness")
+			{
+				if($player->getHappiness() >= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "money")
+			{
+				if($player->getMoney() >= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "education")
+			{
+				if($player->getEducation() >= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "military")
+			{
+				if($player->getMilitary() >= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "population")
+			{
+				if($player->getPopulation() >= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+		}
+		else if($condition->getConditionType() == "lessThanValue")
+		{
+			if($condition->getStatsType() == "food")
+			{
+				if($player->getFood() <= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "happiness")
+			{
+				if($player->getHappiness() <= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "money")
+			{
+				if($player->getMoney() <= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "education")
+			{
+				if($player->getEducation() <= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "military")
+			{
+				if($player->getMilitary() <= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+			else if($condition->getStatsType() == "population")
+			{
+				if($player->getPopulation() <= $condition->getValue())
+				{
+					return true;
+				}
+				return false;
+			}
+		}
+	}
+
+	protected function getStatType()
+	{
+
+	}
+
+	protected function activateEffect($effect, $player)
+	{
+		$player->getTown()->setFood($player->getTown()->getFood() + $effect->getFood());
+		$player->getTown()->setHappiness($player->getTown()->getHappiness() + $effect->getHappiness());
+		$player->getTown()->setMoney($player->getTown()->getMoney() + $effect->getMoney());
+		$player->getTown()->setEducation($player->getTown()->getEducation() + $effect->getEducation());
+		$player->getTown()->setMilitary($player->getTown()->getMilitary() + $effect->getMilitary());
+		$player->getTown()->setPopulation($player->getTown()->getPopulation() + $effect->getPopulation());
+
+		if($effect->getCardsToRemove() > 0)
+		{
+			for($i = 0; $i < $effect->getCardsToRemove(); $i++)
+			{
+				$player->discardCard(mt_rand(0, (count($player->toolCards) - 1)));
+			}
+		}
 	}
 
 	public function changePlayerOrder()//protected
@@ -157,8 +441,19 @@ class World
 
 	public function takeAnEventCard()
 	{
-		$this->currentEventCard = $this->eventDeck[0];
-		unset($this->eventDeck[0]);
-		$this->eventDeck = array_values($this->eventDeck);
+		if(count($this->eventDeck) < 1)
+		{
+			$this->sortEventDeck();
+
+			$this->currentEventCard = $this->eventDeck[0];
+			unset($this->eventDeck[0]);
+			$this->eventDeck = array_values($this->eventDeck);
+		}
+		else
+		{
+			$this->currentEventCard = $this->eventDeck[0];
+			unset($this->eventDeck[0]);
+			$this->eventDeck = array_values($this->eventDeck);
+		}
 	}
 }
