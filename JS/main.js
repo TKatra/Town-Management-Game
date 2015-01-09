c734acb15803aff07ca59545293cb08d216e1060
TKatra <tobiaskatra@hotmail.com>
2015-01-09 15:44:31 +0100

    Made the main.js file able to send and recieve data from PHP with AJAX.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,35 @@
 $(function ()
 {
-	
+	var requestData = {
+		commandLine:""
+	};
+	function siteStartup()
+	{
+		console.log("dthdryyrdtr");
+		requestData.commandLine = "preGameBuild";
+		console.log(requestData);
+		contactPHP(requestData, temp);
+	}
+
+	function contactPHP(request, successFunction)
+	{
+		console.log(request);
+		$.ajax({
+			url:"PHP/main.php",
+			dataType: "json",
+			data: request,
+			success: successFunction,
+			error:function(data)
+			{
+				console.log(data.responseText);
+			}
+		});
+	}
+
+	function temp(data)
+	{
+		console.log(data);
+	}
+
+	siteStartup();
 });
