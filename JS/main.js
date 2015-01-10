f3aa4ebfb04719007af582bec6eec39692b35ab7
TKatra <tobiaskatra@hotmail.com>
2015-01-10 14:13:30 +0100

    made code that appends clickable sections with towns

--- JS/main.js
+++ JS/main.js
@@ -26,7 +26,33 @@ $(function ()
 
 	function buildTownList(townData)
 	{
-		console.log("townData: " + townData["towns"][0]);
+		console.log("townData" + townData);
+		console.log("townData(JSON.stringify): " + JSON.stringify(townData));
+		console.log("townData.towns[0].type: " + townData.towns[0].type);
+		console.log("townData.towns.length: ", townData.towns.length);
+
+		for (var i = 0; i < townData.towns.length; i++)
+		{
+			var newTown = $("<section>");
+			newTown.data("index", i);
+			newTown.addClass("town-list-item noselect");
+			newTown.click(selectTownClick);
+
+			newTown.append($("<h3>").text(townData.towns[i].type));
+			newTown.append($("<p>").text("Food: " + townData.towns[i].food));
+			newTown.append($("<p>").text("Happiness: " + townData.towns[i].happiness));
+			newTown.append($("<p>").text("Money: " + townData.towns[i].money));
+			newTown.append($("<p>").text("Education: " + townData.towns[i].education));
+			newTown.append($("<p>").text("Military: " + townData.towns[i].military));
+
+			$(".pop-up.pre-game .town-list").append(newTown);
+		}
+	}
+
+	function selectTownClick()
+	{
+		$(".town-list-item.selected").removeClass("selected");
+		$(this).addClass("selected");
 	}
 
 	siteStartup();
